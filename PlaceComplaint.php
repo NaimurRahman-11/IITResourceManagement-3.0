6634052526f4f03078871fae5df93884adad3854
Naimur Rahman <nirzon.naim@gmail.com>
2022-08-30 02:07:30 -0400

    Database Connected with place complaint

---
+++ PlaceComplaint.php
@@ -0,0 +1,21 @@
+<?php
+include('DatabaseConnection.php');
+
+if (isset($_POST["submit"])){
+   
+    $Student_ID = $_POST["StudentID"];
+    $PC_ID = $_POST["PCID"];
+    $Description = $_POST["description"];
+    
+    
+    $sql = "INSERT INTO placecomplaint (Student_ID, PC_ID, Description) VALUES ('$Student_ID', '$PC_ID', '$Description')";
+
+    if(mysqli_query($conn, $sql)){
+        echo "Inserted";
+    }
+    else {
+        echo "Not Inserted";
+    }
+}
+
+?>
