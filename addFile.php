87c47b1d7f8fa603e851f7eca561f3d9179cabb8
Joy-extreme <joybhowmick173@gmail.com>
2022-09-09 21:49:57 +0600

    added fileinfo add alert for fail or success

--- addFile.php
+++ addFile.php
@@ -9,6 +9,17 @@ $fileLocation=$_POST['FileLocation'];
 $sql= "insert into file (`FileName`,`FileLocation`)
 values('$fileName','$fileLocation')";
 
-mysqli_query($conn,$sql);
+if(mysqli_query($conn, $sql)){
+       
+    echo '<script>alert("Information about a File is added successfully!");
+      location="ManageFiles.html";
+      </script>';
+      
+  }
+  else {
+    echo '<script>alert("failed to add Information about a File!");
+    location="ManageFiles.html";
+    </script>';
+  }
 
 ?>
