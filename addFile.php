e8dca9ce0139a81bc54380fcea1cd1935989743d
Joy-extreme <joybhowmick173@gmail.com>
2022-09-10 21:19:54 +0600

    fixed some errors

--- addFile.php
+++ addFile.php
@@ -12,13 +12,13 @@ values('$fileName','$fileLocation')";
 if(mysqli_query($conn, $sql)){
        
     echo '<script>alert("Information about a File is added successfully!");
-      location="ManageFiles.html";
+      location="ManageFiles.php";
       </script>';
       
   }
   else {
     echo '<script>alert("failed to add Information about a File!");
-    location="ManageFiles.html";
+    location="ManageFiles.php";
     </script>';
   }
 
