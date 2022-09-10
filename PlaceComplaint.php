b6124b10793af2714fac110ac367ea331b139679
Joy-extreme <joybhowmick173@gmail.com>
2022-09-11 01:52:57 +0600

    added pc id and roll number verification

--- PlaceComplaint.php
+++ PlaceComplaint.php
@@ -9,30 +9,49 @@ if (isset($_POST["submit"])){
     $flag= false;
     
     $fetchSql= "select * from pcassignedlist";
-    $fetchResult=mysqli_query($conn, $fetchSql)
+    $fetchResult=mysqli_query($conn, $fetchSql);
     
     while($row=mysqli_fetch_assoc($fetchResult))
     {
-      if(($row['PC_Id']==$PC_ID) && ($row['Assigned_roll']==$PC_ID))
+      if(($row['PC_Id']==$PC_ID) && validateRoll($row['Assigned_roll'],$Student_ID))
       {
+        $flag= true;
         $insertsql = "INSERT INTO placecomplaint (Student_ID, PC_ID, Description) 
         VALUES ('$Student_ID', '$PC_ID', '$Description')";
-        mysqli_query($conn, $insertsql)
+        mysqli_query($conn, $insertsql);
         echo '<script>alert("Complaint Placed Succesfully!");
         location="StudentPage.html";
         </script>';
-
+        break;
       }
-        
-    
     }
+    if(!$flag)
+    {
+      echo '<script>alert("your roll number or pc id is wrong!");
+        location="StudentPage.html";
+        </script>';
     
-    else {
-        
     }
     
 }
+function validateRoll($rollList, $roll)
+{
+  $flag= false;
+  $rollListArray[]=explode(", ",$rollList);
+  for($i=0; $i<count($rollListArray); $i++)
+  {
+    $string=$rollListArray[$i];
+    if(strcasecmp($string,$roll) == 0)
+    {
+      $flag= true;
+      break;
+    }
+  }
 
+  return $flag;
+
+
+}
 ?>
 
 
