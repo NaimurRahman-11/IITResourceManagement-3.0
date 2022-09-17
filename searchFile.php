1558d95778173620c2979a3048c1006adbccf6e2
Naimur Rahman <nirzon.naim@gmail.com>
2022-09-17 01:05:50 -0400

    Updated search file

--- searchFile.php
+++ searchFile.php
@@ -25,6 +25,38 @@
     <a href="OfficeAssistantPage.html"><p class="home"><i class="fa-solid fa-house-user"></i> Home</p> </a> 
     <a href="logInPage.html"><p class="log-out"><i class="fa-solid fa-right-from-bracket"></i> log out</p> </a>
   </div>
+
+  <form action="UpdateFileLocation.php" method="post">
+    <div class="modal fade" id="exampleModal3" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+
+          <div class="mb-3">
+                <label for="YourID" class="col-form-label">File ID:</label>
+                <input type="text" class="form-control" id="FileName" name="fileID">
+              </div>
+
+            
+              <div class="mb-3">
+                <label for="YourID" class="col-form-label">Enter Updated File Location:</label>
+                <input type="text" class="form-control" id="FileLocation" name="FileLocation">
+              </div>
+
+            
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <input type="submit" class="btn btn-primary" value="Update" name="Search" id="Search">
+          </div>
+        </div>
+      </div>
+    </div>
+   </form>
     
 
 <table>
@@ -33,6 +65,9 @@
   <th>Id</th>
   <th>File Name</th>
   <th>File Location</th>
+  <th>File Category</th>
+  <th>Date</th>
+  <th>Action</th>
   
 </tr>
 
@@ -57,12 +92,20 @@
           $id=$row['id'];
           $FileName=$row['FileName'];
           $FileLocation=$row['FileLocation'];
+          $FileCategory=$row['FileCategory'];
+          $Date=$row['Date'];
               
   ?>
           <tr>
               <td><?php echo $id; ?></td>
               <td><?php echo $FileName; ?> </td>
-              <td><?php echo $FileLocation; ?></td>                    
+              <td><?php echo $FileLocation; ?></td>
+              <td><?php echo $FileCategory; ?></td>
+              <td><?php echo $Date; ?></td>
+              <td>
+                      <a href="DeleteFileInfo.php?fileId=<?php echo $id; ?>" class="delete-button">Delete</a>
+                      <a href="UpdateFileLocation.php?fileId=<?php echo $id; ?>" class="update-button" data-bs-toggle="modal" data-bs-target="#exampleModal3">UpdateLocation</a>
+                    </td>                    
           </tr>
       
   <?php 
