10b11420f79052560ff1bd56894271db915b3f98
Joy-extreme <joybhowmick173@gmail.com>
2022-09-18 13:23:33 +0600

    added submit request

--- ItemRequest.php
+++ ItemRequest.php
@@ -1,31 +1,37 @@
 <?php
+session_start();
+$itemRequestArray=array();
+$itemQuantityArray=array();
 
-// $str= 'penQuantitybox';
-// if(isset($_POST[$str]))
-// {
-//     echo $_POST[$str].'<br>';
-// }
-
-$a[]= 'joy';
-$a[]= 'mou';
-$a[]= 'mita';
-
-for($i=0;$i<count($a);$i++)
+if(isset($_POST['penQuantitybox']))
 {
-    echo $a[$i]."<br>";
+    array_push($itemRequestArray,'pen');
+    array_push($itemQuantityArray,$_POST['penQuantitybox']);
+}
+if(isset($_POST['markerQuantitybox']))
+{
+    array_push($itemRequestArray,'marker');
+    array_push($itemQuantityArray,$_POST['markerQuantitybox']);
+}
+if(isset($_POST['A4PaperQuantitybox']))
+{
+    array_push($itemRequestArray,'A4 Paper');
+    array_push($itemQuantityArray,$_POST['A4PaperQuantitybox']);
+}
+if(isset($_POST['tissueQuantitybox']))
+{
+    array_push($itemRequestArray,'tissue box');
+    array_push($itemQuantityArray,$_POST['tissueQuantitybox']);
+}
+if(isset($_POST['airFreshnerQuantitybox']))
+{
+    array_push($itemRequestArray,'Air Freshner');
+    array_push($itemQuantityArray,$_POST['airFreshnerQuantitybox']);
 }
 
-// if(isset($_POST['penQuantitybox']))
-// {
-//     echo $_POST['penQuantitybox'].'<br>';
-// }
-// if(isset($_POST['penQuantitybox']))
-// {
-//     echo $_POST['penQuantitybox'].'<br>';
-// }
-// if(isset($_POST['penQuantitybox']))
-// {
-//     echo $_POST['penQuantitybox'].'<br>';
-// }
+$_SESSION['itemRequestArray']=$itemRequestArray;
+$_SESSION['itemQuantityArray']=$itemQuantityArray;
+
+//include('validateItemRequest.php');
 
 ?>
