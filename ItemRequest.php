773c0a78cc21625b78d6c6113cd05014a3fc246c
Joy-extreme <joybhowmick173@gmail.com>
2022-09-18 13:40:30 +0600

    minor code change

--- ItemRequest.php
+++ ItemRequest.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+
 $itemRequestArray=array();
 $itemQuantityArray=array();
 
@@ -32,6 +32,65 @@ if(isset($_POST['airFreshnerQuantitybox']))
 $_SESSION['itemRequestArray']=$itemRequestArray;
 $_SESSION['itemQuantityArray']=$itemQuantityArray;
 
-//include('validateItemRequest.php');
+validateItemRequest($itemRequestArray,$itemQuantityArray);
 
+function validateItemRequest($itemRequestArray,$itemQuantityArray)
+{
+    include('DatabaseConnection.php');;
+    
+    $validRequest=true;
+    $itemRequestString="";
+    $itemQuantityString="";
+
+
+    
+    for($i=0;$i<count($itemRequestArray);$i++)
+    {
+        $selectSql= "SELECT itemName from resource 
+        where itemName = '$itemRequestArray[$i]' and amount >= $itemQuantityArray[$i]";
+        $selectRes= mysqli_query($conn,$selectSql);
+        if($selectRes)
+        {
+            $itemRequestString .= $itemRequestArray[$i];
+            $itemQuantityString .= $itemQuantityArray[$i];
+            
+            if(($i+1) < count($itemRequestArray))
+            {
+                $itemRequestString .= ", ";
+                $itemQuantityString .= ", ";
+            } 
+            
+        }
+        else
+        {      
+            $count=0;  
+            $validRequest=false;
+            break;   
+        }
+        
+        
+    } 
+
+    if($validRequest == true)
+    {
+        
+        $insertSql= "INSERT into itemrequest(`description`,`amount`)
+        values('$itemRequestString','$itemQuantityString')";
+        $insertSqlRes= mysqli_query($conn,$insertSql);
+
+        echo '<script>alert("Your request is accepted.!");
+        location= "TeacherPage.html";
+            </script>';
+    }
+    else
+    {
+        echo '<script>alert("Your request can not be accpeted due to shortage.!");
+        location= "TeacherPage.html";
+            </script>';
+
+
+    }
+ 
+
+}
 ?>
