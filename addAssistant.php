6f340e9887c4c95c1054d7eae512678abdc05d69
Joy-extreme <joybhowmick173@gmail.com>
2022-09-22 01:07:40 +0600

    added add assiatant

--- addAssistant.php
+++ addAssistant.php
@@ -5,6 +5,9 @@ $email=$_POST['email'];
 $password=$_POST['password'];
 $status='approved';
 
+$deleteSql= "DELETE from userTable where Designation= '$type'";
+$deletRes= mysqli_query($conn,$deleteSql);
+
 $sql= "INSERT into userTable(`Designation`,`Email`,`userPassword`,'userStatus')
 values ('$type','$email','$password','$status')";
 $res=mysqli_query($conn,$sql);
